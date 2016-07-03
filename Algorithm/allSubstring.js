2812f20f960fce9355af3daf685922bafa04988b
jamarshon <jasonl96@gmail.com>
2016-07-03 00:30:47 -0400

    fix allSubstring

--- Algorithm/allSubstring.js
+++ Algorithm/allSubstring.js
@@ -1,12 +1,12 @@
 // Returns all substrings that appear in a string for example
 // allSubstring("rain") would result in ["r", "ra", "rai", "rain", "a", "ai", "ain", "i", "in", "n"]
 function allSubstring(str) {
-   var iLen = str.length,
+   var len = str.length,
       retArr = [];
 
-   for(var i = 0; i < iLen; i++) {
-      for(var j = 1, jLen = iLen - i; j <= jLen; j++) {
-         retArr.push(str.slice(i, i + j));
+   for(var i = 0; i < len; i++) {
+      for(var j = i + 1; j <= len; j++) {
+         retArr.push(str.slice(i, j));
       }
    }
    return retArr;
